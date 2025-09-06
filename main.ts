e0d748c0ba5d190cb6ca07ec3347aec3329e0b68
guigomaster01 <contatorodrigorodrigues@gmail.com>
2025-09-06 05:57:52 -0300

    Checked Swagger global function

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
@@ -18,6 +18,17 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Cinemais API')
+    .setDescription(
+      'API para gerenciar catálogo de filmes/séries e favoritos de usuários.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
   console.log('🚀 API rodando em http://localhost:3000');
